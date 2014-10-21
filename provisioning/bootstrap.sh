c3d2e179b1c91810b08faa554a8f55af71f22e08
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2014-10-21 20:45:02 +0200

    Adding rules for guniconr & refactor with Vars

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,12 +1,12 @@
-echo "Beggining provisioning - Updating Ubuntu Repos"
+#echo "Beggining provisioning - Updating Ubuntu Repos"
 
 # Updating repos
-apt-get update
 
 #Installing Git
-if [[ !$GIT_INSTALLED ]]; then
-    apt-get -y install git
+#if [[ !$GIT_INSTALLED ]]; then
+#    apt-get update
+#    apt-get -y install git
 
-    echo "export GIT_INSTALLED=1" >> .bashrc
-    export GIT_INSTALLED=1
-fi
+#    echo "export GIT_INSTALLED=1" >> .bashrc
+#    export GIT_INSTALLED=1
+#fi
